f8530a2c20f7edb62e9ba53aa86386bb75b18767
joe rizk <joerizk2@hotmail.com>
2022-02-05 21:45:46 +0200

    created prediction table

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const api_url = "https://dog.ceo/api/breeds/image/random";
 
-function GetName() {
-  let input_name = document.getElementById("name").value;
-  return input_name;
-}
-
 // Defining async function
 async function GetApi(url) {
   // Storing response
@@ -18,18 +13,39 @@ async function GetApi(url) {
 GetApi(api_url);
 
 async function GetAge(url) {
-  // Storing response
   const response = await fetch(url);
-
-  // Storing data in form of JSON
   const data = await response.json();
-  document.getElementById("prediction").innerHTML = data.age;
+  document.getElementById("age").innerHTML = data.age;
 }
+
+async function GetGender(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  document.getElementById("gender").innerHTML = data.gender;
+}
+
+async function GetNationality(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  document.getElementById(
+    "nationality"
+  ).innerHTML = `${data.country[0].country_id}/ ${data.country[1].country_id}/ ${data.country[2].country_id}`;
+}
+
+function GetName() {
+  let input_name = document.getElementById("name").value;
+  return input_name;
+}
+
 function Predict() {
   var name = GetName();
-  console.log(name);
+  document.getElementById("name-display").innerHTML = name;
   var age_url = "https://api.agify.io/?name=" + name;
   GetAge(age_url);
+  var gender_url = "https://api.genderize.io/?name=" + name;
+  GetGender(gender_url);
+  var nationality_url = "https://api.nationalize.io/?name=" + name;
+  GetNationality(nationality_url);
 }
 
 document.getElementById("predict-button").addEventListener("click", Predict);
