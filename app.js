996717e7d978b478147c491848586868eccbc688
joe rizk <joerizk2@hotmail.com>
2022-02-06 17:01:47 +0200

    cleaning code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const api_url = "https://dog.ceo/api/breeds/image/random";
 
-// async function
+// ----- async function that fetches a random dog picture --------
 async function GetApi(url) {
   // To store response
   const response = await fetch(url);
@@ -9,23 +9,27 @@ async function GetApi(url) {
   document.getElementById("dog-img").src = data.message;
 }
 
+// ----- Get The name inserted ------
 function GetName() {
   let input_name = document.getElementById("name").value;
   return input_name;
 }
 
+// ------- Get the Predicted Age --------
 async function GetAge(url) {
   const response = await fetch(url);
   const data = await response.json();
   document.getElementById("age").innerHTML = data.age;
 }
 
+// ------- Get the Predicted Gender --------
 async function GetGender(url) {
   const response = await fetch(url);
   const data = await response.json();
   document.getElementById("gender").innerHTML = data.gender;
 }
 
+// ------- Get the Predicted Nationality --------
 async function GetNationality(url) {
   const response = await fetch(url);
   const data = await response.json();
@@ -34,6 +38,7 @@ async function GetNationality(url) {
   ).innerHTML = `${data.country[0].country_id}, ${data.country[1].country_id} and ${data.country[2].country_id}`;
 }
 
+// -------- Prediction function on click ---------+
 function Predict() {
   var name = GetName();
   var age_url = "https://api.agify.io/?name=" + name;
